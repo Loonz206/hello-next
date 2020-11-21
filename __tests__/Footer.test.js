cfc7e1080fd150d7031e242792f2498d8023bc02
Lenny Peters <mr.lennypeters@gmail.com>
2020-11-20 20:45:08 -0800

    test: 💍 updated to remove debug

--- __tests__/Footer.test.js
+++ __tests__/Footer.test.js
@@ -7,9 +7,8 @@ import Footer from "../src/components/Footer";
 describe("Footer Component", () => {
   test("should have the copyright in the footer", () => {
     const date = new Date().getFullYear();
-    const { getByText, debug } = render(<Footer />);
+    const { getByText } = render(<Footer />);
     // screen.debug() don't forget to import screen
-    debug();
     expect(getByText(`Next Blog | Copyright 2020-${date}`)).toBeInTheDocument();
   });
 });
