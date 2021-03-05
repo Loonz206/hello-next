7e9601d68ce127eb8682b3e1e70e5f8f06532e88
Lenny Peters <mr.lennypeters@gmail.com>
2021-03-04 22:01:00 -0800

    refactor: 💡 Post.js
    added date transformation because the string looks like shit

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,11 +2,14 @@ import React from "react";
 
 // eslint-disable-next-line react/prop-types
 const Post = ({ date, title, description }) => {
+  const newDate = new Date(date).toUTCString();
+  const dateString = newDate.split(" ").slice(0, 4).join(" ");
+
   return (
     <div className="container">
       <div className="text">
         <h2>{title}</h2>
-        <h4>{date}</h4>
+        <h4>{dateString}</h4>
         <p>{description}</p>
       </div>
     </div>
