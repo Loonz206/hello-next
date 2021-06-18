b971fdbda9204a8c46e64614b9e1d062dc2711e6
Lenny Peters <mr.lennypeters@gmail.com>
2021-06-17 20:51:15 -0700

    feat: 🎸 added image to the 404 page
    naked gun 2 gif added

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Head from "next/head";
+import Image from "next/image";
 import Layout from "../src/components/Layout";
 
 const Custom404 = () => {
@@ -13,6 +14,12 @@ const Custom404 = () => {
       <Layout>
         <h1>404 Error</h1>
         <p>Page not found.</p>
+        <Image
+          width={720}
+          height={405}
+          src="https://media.giphy.com/media/13d2jHlSlxklVe/giphy.gif"
+          alt="Nothing to see here | 404 Page"
+        />
       </Layout>
     </>
   );
