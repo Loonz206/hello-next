a8ebf85f24514b82bd461b60c5a244af646f7ab6
Lenny Peters <mr.lennypeters@gmail.com>
2021-06-25 20:13:50 -0700

    test: 💍 updated to have coverage for main

---
+++ src/__tests__/Main.test.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { render, cleanup, act } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+import Main from "../components/Main";
+
+describe("Main", () => {
+  // cleanup
+  afterEach(cleanup);
+
+  test("toggles the navigation", () => {
+    // assemble
+    let children;
+    const { container } = render(<Main />);
+    expect(container.querySelector("#content")).toBeInTheDocument();
+    expect(container.querySelector(".main")).toBeInTheDocument();
+    // act
+    act(() => {
+      children = render(
+        <Main>
+          <h1>Hello Children</h1>
+        </Main>
+      );
+    });
+    // assert
+    expect(children.getByText("Hello Children")).toBeInTheDocument();
+  });
+});
